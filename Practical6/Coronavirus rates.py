94e0620a38b01424e0358e887dd43821ff6bcc25
jianzhang-lu <jianzhang.20@intl.zju.edu.cn>
2021-03-24 00:41:17 +0800

    add 2 files （Practical7）

--- Practical6/Coronavirus rates.py
+++ Practical6/Coronavirus rates.py
@@ -1,22 +1,3 @@
-import matplotlib.pyplot as plt
-x={'USA':29862124, 'Inida':11285561, 'Brazil':11205972, 'Russia':4360823, 'UK':4234924}
-# create a dictionary to include all data
-labels=x.keys()
-print(type(labels))
-# create labels to represent all keys in dictionary 'x'.
-sizes=x.values()
-# create sizes to represent all values in dictionary 'x'
-explode=[0.1,0,0,0,0]
-# explode one of the countries to make it take apart from the circle.
-plt.pie(sizes, labels=labels, explode=explode, autopct='%1.1f%%', startangle=90)
-# make the pie and let its sizes be values of x and let lebels be keys of x.
-plt.axis('equal')
-# to make the pie look like a circle
-plt.legend()
-# add a legend for a better pie
-plt.title("Coronavirus_rates")
-# the name of the pie
 plt.show()
 # to see the pie
-print(x) 
 # print the dictionary 
