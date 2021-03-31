ae6158125fab7c4a9b1ad64e04f6b14c2f10a372
jianzhang-lu <jianzhang.20@intl.zju.edu.cn>
2021-03-31 10:58:31 +0800

    add all files

---
+++ Practical8/get_unknown_function.py
@@ -0,0 +1,45 @@
+import re
+file1=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
+file2=open("unknown_function.fa","w")
+file_read=file1.read()
+x=re.split('>',file_read)
+for i in x:
+    if 'unknown function' in i:
+        a=re.findall(r'gene:(.+?)\s',i)
+        new_item=re.sub(r'.+]','',i)
+        b=re.sub(r'\n','',new_item)
+        c=len(b)
+        for e in a:
+            file2.write(e)
+            file2.write('\t\t\t')
+            file2.write(str(c))
+            file2.write(new_item)
+file1.close()
+file2.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
