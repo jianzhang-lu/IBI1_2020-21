3b531b2ce8886ffaa6dc5e07b45fd27ac076bae1
jianzhang-lu <jianzhang.20@intl.zju.edu.cn>
2021-04-09 20:44:38 +0800

    correct

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -9,4 +9,4 @@ n=84
 r=float(input("the rate of reproduction of a virus:"))
 for i in range(0,5):
     n=n*r+n
-print("The rate of reproduction of a virus is",r,", and the total number of individuals infected after 5 generations is "
+print("The rate of reproduction of a virus is",r,", and the total number of individuals infected after 5 generations is",n)
